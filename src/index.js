a12e6ea68bbbbbdc8d6756821fa2e581599b377e
maskzh <maskzh@gmail.com>
2016-06-02 11:41:50 +0800

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import _borders from './lib/_borders'
-import _colors from './lib/_colors'
-import _flex from './lib/_flex'
-import _typography from './lib/_typography'
-import _whitespace from './lib/_whitespace'
-import _utilities from './lib/_utilities'
+import _borders from './_borders'
+import _colors from './_colors'
+import _flex from './_flex'
+import _typography from './_typography'
+import _whitespace from './_whitespace'
+import _utilities from './_utilities'
 
 export default Object.assign({}, _borders, _colors, _flex, _typography, _whitespace, _utilities)
